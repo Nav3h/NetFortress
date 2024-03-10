f5fcb9e0b6343bbf222791bfda44cd382bce85b7
Nav3h <naveh.hy@gmail.com>
2024-03-10 15:47:56 -0700

    Enhanced brute force detector via ip and port keys

--- IDS/detectors/brute_force_detector.py
+++ IDS/detectors/brute_force_detector.py
@@ -5,60 +5,44 @@ Module for detecting brute force attacks in network traffic.
 import time
 from collections import defaultdict
 from IDS.utils.common_utils import print_with_timestamp, cleanup_tracker, RED
+from IDS.detectors.attack_detector import AttackDetector
 from collections import deque
 
 TIME_WINDOW = 10
 
-class BruteForceDetector:
+class BruteForceDetector(AttackDetector):
     def __init__(self, threshold):
-        """
-        Initialize the brute force detector with a threshold.
-        """
-        self.threshold = threshold
-        self.brute_force_tracker = {}
+        self.threshold = threshold  
+        self.brute_force_tracker = {} 
 
     def detect(self, packet_data):
-        """
-        Detect potential brute force activities based on packet data.
-        """
-        dst = packet_data.get('dst')
-        dport = packet_data.get('dport')
+        dst_ip = packet_data.get('dst')
+        dst_port = packet_data.get('dport')
+        current_time = time.time()
 
-        if not dst or not dport:
+        if dst_ip is None or dst_port is None:
             return
 
-        if dst not in self.brute_force_tracker:
-            self.brute_force_tracker[dst] = {
-                'count': 0,
-                'last_seen': time.time(),
+        ip_port_key = (dst_ip, dst_port)
+
+        if ip_port_key not in self.brute_force_tracker:
+            self.brute_force_tracker[ip_port_key] = {
+                'count': 0, 
+                'times': deque()
+            }
+        self.brute_force_tracker[ip_port_key]['count'] += 1
+        self.brute_force_tracker[ip_port_key]['times'].append(current_time)
+
+
+        while self.brute_force_tracker[ip_port_key]['times'] and  current_time - self.brute_force_tracker[ip_port_key]['times'][0] > TIME_WINDOW:
+            self.brute_force_tracker[ip_port_key]['times'].popleft()
+            self.brute_force_tracker[ip_port_key]['count'] -= 1
+
+        if self.brute_force_tracker[ip_port_key]['count'] > self.threshold:
+            print(f"[ALERT] Brute force detected on {dst_ip}:{dst_port}")
+
+            self.brute_force_tracker[ip_port_key] = {
+                'count': 0, 
                 'times': deque()
             }
 
-        self.brute_force_tracker[dst]['count'] += 1
-        self.brute_force_tracker[dst]['last_seen'] = time.time()
-        self.brute_force_tracker[dst]['times'].append(time.time())
-
-              
-        if self.brute_force_tracker[dst]['count'] > self.threshold:
-            print_with_timestamp(f"Suspicious brute force activity on {dst}:{dport}", RED)
-            del self.brute_force_tracker[dst]
-
-        self.cleanup_tracker(dst)
-
-
-    def cleanup_tracker(self, dst):
-        """
-        Cleanup old entries from the tracker.
-        """
-        current_time = time.time()
-
-        if dst not in self.brute_force_tracker:
-            self.brute_force_tracker[dst] = {'count': 0, 'times': deque(), 'last_seen': 0}
-
-        times = self.brute_force_tracker[dst]['times']
-
-        while times and current_time - times[0] > TIME_WINDOW:
-            times.popleft()
-
-        if not times:
-            del self.brute_force_tracker[dst]
